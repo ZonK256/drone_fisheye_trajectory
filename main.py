86e533b43cf904fa7486a200d149003d6f3c9ac6
zonk256 <asd05728@gmail.com>
2025-01-30 01:30:38 +0100

    Refactor output matrix rendering to normalize and plot multiple matrices; improve file handling and visualization

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import socket
 import argparse
 import time
-import os
-import datetime
 
 # puścić liczenie
 # 9 macierzy markowa 3x3 wyświetlone jako obrazki z gradientem kolorów reprezentującym częstość występowania danego stanu
@@ -13,22 +11,22 @@ start_time = time.time()
 
 # Argument parser
 parser = argparse.ArgumentParser(description="Fisheye trajectory simulation")
-parser.add_argument(
-    "--velocity_vector",
-    type=int,
-    nargs=2,
-    default=None,
-    help="Specify the velocity vector as two integers, e.g., --velocity_vector 1 2",
-)
-parser.add_argument(
-    "--test_mode",
-    action="store_true",
-    help="Run in test mode (hold C to generate new trajectory, press Q to quit)",
-)
+# parser.add_argument(
+#     "--velocity_vector",
+#     type=int,
+#     nargs=2,
+#     default=None,
+#     help="Specify the velocity vector as two integers, e.g., --velocity_vector 1 2",
+# )
+# parser.add_argument(
+#     "--test_mode",
+#     action="store_true",
+#     help="Run in test mode (hold C to generate new trajectory, press Q to quit)",
+# )
 parser.add_argument(
     "--max_trajectories",
     type=int,
-    default=10000,
+    default=100000,
     help="Number of trajectories to generate, (default: 10000, if 0 then infinite)",
 )
 parser.add_argument(
@@ -39,9 +37,7 @@ parser.add_argument(
 )
 
 ### PARAMETRY ###
-Rpix = 1000  # promień kamery w pikselach
-znany_max_pix_velocity = 1  # to jest wyznaczane na podstawie max_pix_velocity
-# sqrt(2) => ~1.4.... najwyższa prędkość dla wektora 1,1
+Rpix = 128  # promień kamery w pikselach
 
 
 MIN_SEGMENTS_PER_TRAJECTORY = (
@@ -51,33 +47,49 @@ MAX_SEGMENTS_PER_TRAJECTORY = 2000  # Maksymalna liczba segmentów na trajektori
 # 2000 wyglądana wystarczająco dla pix_velocity 1
 
 # Parametry bryły
-radius = 1000  # Promień cylindra i stożka
-height = 1000  # Wysokość cylindra i stożka
+radius = 2500  # Promień cylindra i stożka
+height = 2500  # Wysokość cylindra i stożka
 # cone_height = 1000  # Wysokość stożka
-cone_bottom_radius = 100  # Promień podstawy stożka (na dole)
+cone_bottom_radius = 0  # Promień podstawy stożka (na dole)
 
 AUTOSAVE_INTERVAL = 5000  # Co ile trajektorii wypisać informację o czasie
 
 args = parser.parse_args()
 
-if args.velocity_vector:
-    wektory2D = [args.velocity_vector]
-else:
-    # Generowanie wektorów 2D
-    wektory2D = []
-    for v1 in range(-znany_max_pix_velocity, znany_max_pix_velocity + 1):
-        for v2 in range(-znany_max_pix_velocity, znany_max_pix_velocity + 1):
-            wektory2D.append([v1, v2])
+# if args.velocity_vector:
+#     wektory2D = [args.velocity_vector]
+# else:
+#     # Generowanie wektorów 2D
+#     wektory2D = []
+#     for v1 in range(-znany_max_pix_velocity, znany_max_pix_velocity + 1):
+#         for v2 in range(-znany_max_pix_velocity, znany_max_pix_velocity + 1):
+#             wektory2D.append([v1, v2])
+
+wektory2D = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 0),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+output_matrix = np.zeros((Rpix * 2 + 1, Rpix * 2 + 1, 9), dtype=int)
 
 max_trajectories = 0 if args.max_trajectories < 0 else args.max_trajectories
 
-file_suffix = (
-    "random"
-    if args.velocity_vector is None
-    else f"{args.velocity_vector[0]}.{args.velocity_vector[1]}"
-)
-if args.additional_seed:
-    file_suffix += f"_{args.additional_seed}"
+# file_suffix = (
+#     "random"
+#     if args.velocity_vector is None
+#     else f"{args.velocity_vector[0]}.{args.velocity_vector[1]}"
+# )
+# if args.additional_seed:
+#     file_suffix += f"_{args.additional_seed}"
+
+file_suffix = 0 if not args.additional_seed else args.additional_seed
 
 
 # Funkcja do zapisania stanu generatora do pliku (lub pamięci)
@@ -97,6 +109,25 @@ def load_rng_state(filepath):
 # np.random.seed(int(IP_STR.replace(".", "")))  # Seed do losowania trajektorii
 
 
+def cart2fish_image(x, y, z) -> np.array:
+    # convert cartesian coordinates to spherical and then map to fisheye frame coordinates (Px, Py) in range [0, frame_size]
+
+    azimuth = np.arctan2(y, x)
+    elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
+    # r = np.sqrt(x**2 + y**2 + z**2)
+
+    elevation = np.pi / 2 - elevation
+    fi_max = np.pi  # fisheye FoV in radians
+    r_norm = elevation / (fi_max / 2)
+    Px = r_norm * np.cos(azimuth)
+    Py = r_norm * np.sin(azimuth)
+
+    pixel_x = int(Px * Rpix + Rpix)
+    pixel_y = int(Py * Rpix + Rpix)
+
+    return pixel_x, pixel_y
+
+
 def cart2sph(x, y, z):
     azimuth = np.arctan2(y, x)
     elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
@@ -114,6 +145,7 @@ def fisheye_trajectory(xyz_trajectory: np.array) -> np.array:
     r_norm = elm / (fi_max / 2)
     Px = r_norm * np.cos(az)
     Py = r_norm * np.sin(az)
+
     return np.column_stack((Px, Py))
 
 
@@ -128,98 +160,153 @@ def is_outside_area(xyz) -> bool:
     x, y, z = xyz
     distance_from_center = np.sqrt(x**2 + y**2)
 
-    is_test_mode = args.test_mode
+    # is_test_mode = args.test_mode
 
     if distance_from_center > radius:  # poza okręgiem
         outside_circle += 1
-        if is_test_mode:
-            print(
-                f"End of trajectory due to outside circle (distance: {distance_from_center}, radius: {radius})"
-            )
+        # if is_test_mode:
+        #     print(
+        #         f"End of trajectory due to outside circle (distance: {distance_from_center}, radius: {radius})"
+        #     )
         return True
     if (
         distance_from_center < cone_bottom_radius
     ):  # wewnątrz stożka (wewnętrzny walec dla uproszczenia)
         inside_cone_circle += 1
-        if is_test_mode:
-            print(
-                f"End of trajectory due to inside cone circle (distance: {distance_from_center}, cone_bottom_radius: {cone_bottom_radius})"
-            )
+        # if is_test_mode:
+        #     print(
+        #         f"End of trajectory due to inside cone circle (distance: {distance_from_center}, cone_bottom_radius: {cone_bottom_radius})"
+        #     )
         return True
     if (
         distance_from_center
         < cone_bottom_radius + (radius - cone_bottom_radius) * z / height
     ):  # jeśli jest wewnątrz odwróconego stożka
         inside_cone += 1
-        if is_test_mode:
-            print(
-                f"End of trajectory due to inside cone (distance: {distance_from_center}, cone_bottom_radius: {cone_bottom_radius}, radius: {radius}, height: {height})"
-            )
+        # if is_test_mode:
+        #     print(
+        #         f"End of trajectory due to inside cone (distance: {distance_from_center}, cone_bottom_radius: {cone_bottom_radius}, radius: {radius}, height: {height})"
+        #     )
         return True
     return False
 
 
-def generate_trajectory() -> np.array:
-    len_xyz = 0
+# def generate_trajectory() -> np.array:
+#     len_xyz = 0
 
-    while len_xyz < MIN_SEGMENTS_PER_TRAJECTORY:
-        xyzT = np.zeros((MAX_SEGMENTS_PER_TRAJECTORY, 3))
+#     while len_xyz < MIN_SEGMENTS_PER_TRAJECTORY:
+#         xyzT = np.zeros((MAX_SEGMENTS_PER_TRAJECTORY, 3))
 
-        z = (
-            rng.random() * height * 0.99
-        )  # nie losuj z całej wysokości, dla granicznych przypadków następuje nieprawidłowe zachowanie
+#         z = (
+#             rng.random() * height * 0.99
+#         )  # nie losuj z całej wysokości, dla granicznych przypadków następuje nieprawidłowe zachowanie
 
-        # losuj koordynaty na obwodzie okręgu
-        point_angle = rng.random() * np.pi * 2
-        xyzT[0] = [
-            radius * np.cos(point_angle),
-            radius * np.sin(point_angle),
-            z,
-        ]
+#         # losuj koordynaty na obwodzie okręgu
+#         point_angle = rng.random() * np.pi * 2
+#         xyzT[0] = [
+#             radius * np.cos(point_angle),
+#             radius * np.sin(point_angle),
+#             z,
+#         ]
 
-        trajectory_x, trajectory_y = wektory2D[rng.integers(0, len(wektory2D))]
+#         trajectory_x, trajectory_y = wektory2D[rng.integers(0, len(wektory2D))]
 
-        for i in range(1, MAX_SEGMENTS_PER_TRAJECTORY):
-            xyzT[i] = [
-                xyzT[i - 1][0] + trajectory_x,
-                xyzT[i - 1][1] + trajectory_y,
-                z,
-            ]
+#         for i in range(1, MAX_SEGMENTS_PER_TRAJECTORY):
+#             xyzT[i] = [
+#                 xyzT[i - 1][0] + trajectory_x,
+#                 xyzT[i - 1][1] + trajectory_y,
+#                 z,
+#             ]
 
-            if is_outside_area(xyzT[i]):
-                break
+#             if is_outside_area(xyzT[i]):
+#                 break
 
-        # usuń z listy punkty 0,0,0
-        xyzT = xyzT[~np.all(xyzT == 0, axis=1)]
-        len_xyz = len(xyzT)
+#         # usuń z listy punkty 0,0,0
+#         xyzT = xyzT[~np.all(xyzT == 0, axis=1)]
+#         len_xyz = len(xyzT)
 
-    return xyzT
+#     return xyzT
+
+valid_vectors = 0
+invalid_vectors = 0
+
+
+def simulate_trajectory():
+    global output_matrix, invalid_vectors, valid_vectors
+    # losuj koordynaty na obwodzie okręgu i wysokość
+    # losuj wektor prędkości float z zakresu -1,1 dla poruszania się x i y
+    # dopóki nie wyleci poza dozwolony obszar:
+    #   dodaj do punktu wektor prędkości
+    #   rzutuj na kamere
+    #   porównaj nowy obraz z poprzednim
+    #   jeśli nie ma różnicy, to w w ouput_matrix{0,0} na pozycji punktu dodaj 1
+    #   jeśli jest różnica to określ kierunek i dodaj 1 do odpowiedniego output_matrix
+    angle = rng.random() * np.pi * 2
+
+    x = radius * np.cos(angle)
+    y = radius * np.sin(angle)
+    z = rng.uniform(0, height)
+
+    x_velocity = -1
+    y_velocity = -1
+
+    new_drone_image_position = cart2fish_image(x, y, z)
+    previous_drone_image_position = new_drone_image_position
+
+    # print(f"Starting point: ({x}, {y}, {z}), velocity: ({x_velocity}, {y_velocity})")
+    x += x_velocity
+    y += y_velocity
+    while not is_outside_area((x, y, z)):
+        x += x_velocity
+        y += y_velocity
+        # print(f"Moving to: ({x}, {y}, {z})")
+        new_drone_image_position = cart2fish_image(x, y, z)
+        x_diff = new_drone_image_position[0] - previous_drone_image_position[0]
+        y_diff = new_drone_image_position[1] - previous_drone_image_position[1]
+
+        if (x_diff, y_diff) in wektory2D:
+            output_matrix[
+                new_drone_image_position[0],
+                new_drone_image_position[1],
+                wektory2D.index((x_diff, y_diff)),
+            ] += 1
+            valid_vectors += 1
+
+            previous_drone_image_position = new_drone_image_position
+
+        else:
+            print(
+                f"Invalid direction, velocity vector out of bounds ({x_diff}, {y_diff})"
+            )
+            invalid_vectors += 1
+            break
 
 
 def generate_trajectories_chunk(iterations=AUTOSAVE_INTERVAL):
     global output_matrix
 
+    # for _ in range(iterations):
+    #     xyzT = generate_trajectory()
+    #     # max_trajectory_length = max(max_trajectory_length, len(xyzT))
+
+    #     # Projekcja do kamery fisheye
+    #     fT = fisheye_trajectory(xyzT)
+    #     plt.plot(fT[:, 0], fT[:, 1])
+
+    #     ft_int = np.round(fT * Rpix).astype(int) + Rpix
+    #     # Zliczanie punktów na macierzy wyjściowej
+
+    #     output_matrix[ft_int[:, 0], ft_int[:, 1]] += 1
+
     for _ in range(iterations):
-        xyzT = generate_trajectory()
-        # max_trajectory_length = max(max_trajectory_length, len(xyzT))
-
-        # Projekcja do kamery fisheye
-        fT = fisheye_trajectory(xyzT)
-        plt.plot(fT[:, 0], fT[:, 1])
-
-        ft_int = np.round(fT * Rpix).astype(int) + Rpix
-        # Zliczanie punktów na macierzy wyjściowej
-
-        output_matrix[ft_int[:, 0], ft_int[:, 1]] += 1
+        simulate_trajectory()
 
 
 def autosave_progress():
-
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     # zapisz stan generatora
     save_rng_state(rng, f"rng_{file_suffix}.npy")
     # zapisz macierz wyjściową
-    np.save(f"matrix_{file_suffix}_{timestamp}.npy", output_matrix)
+    np.save(f"matrix_{file_suffix}.npy", output_matrix)
 
 
 def generate_trajectories() -> np.array:
@@ -269,17 +356,12 @@ def generate_trajectories() -> np.array:
 def load_or_initialize():
     global output_matrix, rng
     try:
-        matching_files = [
-            file
-            for file in os.listdir(os.getcwd())
-            if file_suffix in file and file.endswith(".npy")
-        ]
-        latest_file = max(matching_files, key=os.path.getctime)
+        latest_file = f"matrix_{file_suffix}.npy"
         output_matrix = np.load(latest_file)
         print(f"Loaded output matrix {latest_file}")
     except (FileNotFoundError, ValueError):
         print("No output matrix file found, initializing new output matrix")
-        output_matrix = np.zeros((Rpix * 2 + 1, Rpix * 2 + 1), dtype=int)
+        output_matrix = np.zeros((Rpix * 2 + 1, Rpix * 2 + 1, 9), dtype=int)
 
     try:
         rng = load_rng_state(f"rng_{file_suffix}.npy")
@@ -288,52 +370,57 @@ def load_or_initialize():
         print("No rng state file found, generating new rng state")
         IP_STR = socket.gethostbyname(socket.gethostname())
         rng = np.random.default_rng(
-            seed=int(IP_STR.replace(".", "")) + args.additional_seed
+            seed=int(IP_STR.replace(".", "") + str(args.additional_seed))
         )
 
 
-def run_test_mode():
-    fig = plt.figure()
-    plt.axis("equal")
-    plt.grid()
-    plt.title(
-        "Fisheye trajectory simulation (press C to generate new trajectory, Q to quit)"
-    )
+# def run_test_mode():
+#     fig = plt.figure()
+#     plt.axis("equal")
+#     plt.grid()
+#     plt.title(
+#         "Fisheye trajectory simulation (press C to generate new trajectory, Q to quit)"
+#     )
 
-    # plot outside circle (radius, 30% opacity)
-    circle = plt.Circle((0, 0), 1, color="r", alpha=0.3)
-    fig.gca().add_artist(circle)
+#     # plot outside circle (radius, 30% opacity)
+#     circle = plt.Circle((0, 0), 1, color="r", alpha=0.3)
+#     fig.gca().add_artist(circle)
 
-    running = True
+#     running = True
 
-    def on_key(event):
-        global running
-        if event.key == "q":
-            running = False
-            plt.close()
-        elif event.key == "c":
-            xyzT = generate_trajectory()
-            ft = fisheye_trajectory(xyzT)
-            plt.plot(ft[:, 0], ft[:, 1])
-            plt.draw()
+#     def on_key(event):
+#         global running
+#         if event.key == "q":
+#             running = False
+#             plt.close()
+#         elif event.key == "c":
+#             xyzT = generate_trajectory()
+#             ft = fisheye_trajectory(xyzT)
+#             plt.plot(ft[:, 0], ft[:, 1])
+#             plt.draw()
 
-    fig.canvas.mpl_connect("key_press_event", on_key)
+#     fig.canvas.mpl_connect("key_press_event", on_key)
 
-    # Initial trajectory
-    xyzT = generate_trajectory()
-    ft = fisheye_trajectory(xyzT)
-    plt.plot(ft[:, 0], ft[:, 1])
+#     # Initial trajectory
+#     xyzT = generate_trajectory()
+#     ft = fisheye_trajectory(xyzT)
+#     plt.plot(ft[:, 0], ft[:, 1])
 
-    plt.show()
+#     plt.show()
 
 
 if __name__ == "__main__":
     load_or_initialize()
 
-    if args.test_mode:
-        run_test_mode()
-    else:
-        # plt.axis("equal")
-        # plt.grid()
-        generate_trajectories()
-        # plt.show()
+    # if args.test_mode:
+    #     run_test_mode()
+    # else:
+    #     # plt.axis("equal")
+    #     # plt.grid()
+    #     generate_trajectories()
+    #     # plt.show()
+
+    generate_trajectories()
+
+    print(f"Valid vectors: {valid_vectors}")
+    print(f"Invalid vectors: {invalid_vectors}")
--- output_matrix_renderer.py
+++ output_matrix_renderer.py
@@ -2,44 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-vectors = ["-1.-1", "-1.0", "-1.1", "0.-1", "random", "0.1", "1.-1", "1.0", "1.1"]
-# vectors = ["-1.-1"]
+from main import wektory2D  # import żeby zachować zgodność z nazwami zmiennych
 
-matrix_files = [f for f in os.listdir() if "matrix" in f]
+matrix_files = [f for f in os.listdir() if "matrix" in f and f.endswith(".npy")]
 print(matrix_files)
 
-for vector in vectors:
-    # find all files with current vector
-    matricies = [f for f in matrix_files if f"matrix_{vector}" in f]
+# matrix = np.zeros((9, 9, 9), dtype=np.int32)
 
-    if len(matricies) == 0:
-        print(f"Missing matrix for {vector} vector")
-        continue
+# matrix[4, 4, 0] = 1
+# matrix[4, 4, 1] = 1
+# matrix[4, 4, 2] = 1
+# matrix[4, 4, 3] = 1
+# matrix[4, 4, 4] = 1
+# matrix[4, 4, 5] = 1
+# matrix[4, 4, 6] = 1
+# matrix[4, 4, 7] = 1
+# matrix[4, 4, 8] = 1
 
-    print(f"Found {len(matricies)} matricies for {vector} vector: {matricies}")
-    matrix = np.load(matricies[0])
-    for m in matricies[1:]:
-        matrix += np.load(m)
 
-    # normalize
-    matrix = matrix / matrix.max()
-    # rotate matrix 90 degrees, so it looks like in the simulation
-    matrix = np.rot90(matrix, k=1)
+matrix = np.load(matrix_files[0])
 
-    Rpix = matrix.shape[0] // 2
+# normalize 9 matrices to the total sum of 1
+sum_matrix = np.sum(matrix)
+normalized_matricies = np.zeros(matrix.shape, dtype=np.float32)
+normalized_matricies = matrix / sum_matrix
 
-    # if point is outside of the circle, set it to unvisible
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            if (i - Rpix) ** 2 + (j - Rpix) ** 2 > Rpix**2:
-                matrix[i, j] = np.nan
+Rpix = matrix[:, :, 0].shape[0] // 2
 
-    plt.figure()
-    plt.title(f"[{vector.replace('.', ' ')}] vector (Rpix={Rpix})")
-    plt.axis("equal")
-    plt.imshow(matrix, cmap="plasma")
-    plt.colorbar()
-    plt.show()
+fig = plt.figure()
+# plot all 9 matrices in one figure
+ax_array = fig.subplots(3, 3, sharex=True, sharey=True)
+
+
+for i in range(9):
+    # if point is outside of the circle, remove it (np.nan)
+    # circle is defined by center (Rpix, Rpix) and radius Rpix
+
+    sub_matrix = normalized_matricies[:, :, i]
+    for x in range(sub_matrix.shape[0]):
+        for y in range(sub_matrix.shape[1]):
+            if (x - Rpix) ** 2 + (y - Rpix) ** 2 > Rpix**2:
+                sub_matrix[x, y] = np.nan
+
+    # print(i // 3, i % 3, sub_matrix)
+
+    ax_array[i // 3, i % 3].imshow(
+        sub_matrix,
+        cmap="plasma",
+        interpolation="nearest",
+    )
+
+# add colorbar to the bottom of the figure
+cbar = fig.colorbar(
+    ax_array[0, 0].imshow(normalized_matricies[:, :, 0], cmap="plasma"),
+    ax=ax_array,
+    orientation="horizontal",
+)
+
+plt.show()
 
 
 # dostępne cmap: 'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'
