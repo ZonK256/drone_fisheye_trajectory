e3eb261b54056cb7ce72397a2a2208d028631239
zonk256 <asd05728@gmail.com>
2025-01-30 01:43:04 +0100

    Refactor instance execution logic; update trajectory count and sleep duration

--- run_instances.py
+++ run_instances.py
@@ -1,31 +1,22 @@
 import os
 import datetime
 
-vectors = ["-1 -1", "-1 0", "-1 1", "0 -1", "0 1", "1 -1", "1 0", "1 1"]
+# vectors = ["-1 -1", "-1 0", "-1 1", "0 -1", "0 1", "1 -1", "1 0", "1 1"]
 
-instances_per_vector = 3
-trajecotries_per_instance = 20_000
-secs_to_sleep = 60
+instances = 3
+trajecotries_per_instance = 100_000
+secs_to_sleep = 400
 
 
-command = "screen -dmS {} python3 main.py --velocity_vector {} --max_trajectories {}  --additional_seed {}"
-random_command = (
-    "screen -dmS random{} python3 main.py --max_trajectories {}  --additional_seed {}"
-)
+command = "screen -dmS {} python3 main.py --max_trajectories {} --additional_seed {}"
 
 if __name__ == "__main__":
     while True:
         print(f"Starting new batch at {datetime.datetime.now()}")
 
-        for instance in range(instances_per_vector):
-            # Run random instance
-            seed = instance
-            print(f" Running random instance")
-            os.system(random_command.format(instance, trajecotries_per_instance, seed))
-            for vector in vectors:
-                print(f" Running [{vector}] instance {instance}")
-                screen_name = f"{vector.replace(' ', '_')}_{instance}"
-                os.system(
-                    command.format(screen_name, vector, trajecotries_per_instance, seed)
-                )
+        for i in range(instances):
+            seed = i
+            print(f" Running instance {i}")
+            screen_name = f""
+            os.system(command.format(screen_name, trajecotries_per_instance, seed))
         os.system(f"sleep {secs_to_sleep}")
